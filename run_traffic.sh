45fa5399bbf6f87d33e18d4e90566b8eeb5a468a
Alan Arbizu <alan.arbizu@gmail.com>
2015-09-20 17:47:06 -0700

    fix run_traffic.sh

--- run_traffic.sh
+++ run_traffic.sh
@@ -3,5 +3,6 @@
 # wrapper to invoke the maven-build jar as part of a container process
 jar_name=$(ls traffic/target/traffic*.jar)
 
-print "Starting traffic checker..."
-java -jar $jar_name
+echo "Starting traffic checker..."
+echo "running java -jar ${jar_name}"
+java -jar ${jar_name}"
