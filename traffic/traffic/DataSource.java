9254ba952d298c9eb9cc9bbe8dc5d6a2e9c8f196
Alan <alan.arbizu@gmail.com>
2017-03-20 00:45:36 -0700

    gson/byte array output stream processing, instead of strings/JSONArray

--- traffic/traffic/DataSource.java
+++ traffic/traffic/DataSource.java
@@ -1,61 +1,62 @@
 package traffic;
 
-import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-
+import java.util.logging.Level;
 
 /**
- *
- * Created by alan on 3/18/17.
+ * @author alan
+ * @since 3/19/2017
  */
-public class DataSource {
+class DataSource {
     private static final String DATA_URL = "http://www.sigalert.com/Data/NoCal/1~j/NoCalData.json?cb=25615489";
     private static final String METADATA_URL = "http://cdn-static.sigalert.com/154/Zip/RegionInfo/NoCalStatic.js";
-    private static ArrayList<Integer> roadSections = Lists.newArrayList(2645, 2646, 2647, 2648, 2649, 2650, 2651, 2652, 2653, 2654);
+    private static final ArrayList<Integer> roadSections = Lists.newArrayList(2645, 2646, 2647, 2648, 2649, 2650, 2651, 2652, 2653, 2654);
     private final static String SPEEDS = "speeds";
     private final static String INCIDENTS = "incidents";
     private final static String SENSOR_NAMES = "SensorNames";
-    private Splitter semicolonSplitter = Splitter.on(";");
-    private Splitter equalsSplitter = Splitter.on("=");
-    
-    private AutoflushingLogger logger;
-    private JsonParser json = new JsonParser();
+    private final Splitter semicolonSplitter = Splitter.on(";");
+    private final Splitter equalsSplitter = Splitter.on("=");
+    private final JsonParser json = new JsonParser();
+    private final Gson gson;
+    private final AutoflushingLogger logger;
     
     DataSource(AutoflushingLogger logger) {
         this.logger = logger;
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(TrafficDatum.class, new TrafficDatumTypeAdapter());
+        this.gson = builder.create();
     }
     
     private enum SourceType {
         DATA(DATA_URL),
         METADATA(METADATA_URL);
         
-        private String url;
+        private final String url;
         SourceType(String url) { this.url = url; }
         public String getUrl() {  return this.url;  }
     }
     
     private TrafficDataReader getReaderFor(SourceType type) {
         if (type == SourceType.METADATA) {
+            //TODO -- METADATA reader should have a fall back for when the static json obj is unavailable (see 403's sometimes)
             return FileDataReader.create("static-js-resources", logger);
         } else {
             return WebDataReader.create(type.getUrl(), logger);
         }
     }
     
-    LinkedHashMap<String, String> collect() {
+    ByteArrayOutputStream collect() {
         TrafficDataReader dataReader = getReaderFor(SourceType.DATA).init().read();
         TrafficDataReader metadataReader = getReaderFor(SourceType.METADATA).init().read();
     
         LinkedHashMap<Integer,TrafficDatum> data = new LinkedHashMap<>();
-        LinkedHashMap<String,String> retVal = null;
         try {
             dataReader.initReader();
             while (dataReader.hasNext()) {
@@ -81,6 +82,9 @@ public class DataSource {
             }
             dataReader.close();
             
+            // log the traffic data to csv format, asynchronously
+            TrafficLogging.process(data);
+            
             metadataReader.initReader();
             while (metadataReader.hasNext()) {
                 String line = metadataReader.nextLine();
@@ -90,21 +94,69 @@ public class DataSource {
                     if (segmentParts.size() < 2) continue;
                     if (segmentParts.get(0).endsWith(SENSOR_NAMES)) {
                         JsonElement element = json.parse(segmentParts.get(1));
-                        roadSections.forEach( locator -> {
-                            data.get(locator).setLocationName(element.getAsJsonArray().get(locator).toString());
-                        });
+                        roadSections.forEach( locator -> data.get(locator).setLocationName(element.getAsJsonArray().get(locator).getAsString()));
                     }
                 }
             }
             metadataReader.close();
-    
-            retVal = data.values()
-                    .stream()
-                    .collect(Collectors.toMap(TrafficDatum::getLocationName, TrafficDatum::toString, (v1, v2) -> v1, LinkedHashMap::new));
-            
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Preconditions.checkNotNull(retVal);
+        return toJson(data);
+    }
+    
+    private ByteArrayOutputStream toJson(LinkedHashMap<Integer,TrafficDatum> rawData) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream(1024);
+        try (
+            Writer w = new BufferedWriter(new OutputStreamWriter(b))
+        ) {
+            JsonArray jsonArr = rawData.values()
+                    .stream()
+                    .map(datum -> {
+                        JsonArray a = new JsonArray();
+                        a.add(datum.getLocationName());
+                        a.add(datum.speedAndMaybeIncident());
+                        return a;
+                    })
+                    .collect(JsonArray::new, JsonArray::add, JsonArray::addAll);
+            gson.toJson(jsonArr, w);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "writing json", new Object[] { e });
+        }
+        return b;
+    }
+    
+    static class TrafficDatumTypeAdapter extends TypeAdapter<TrafficDatum> {
+        @Override
+        public void write(JsonWriter jsonWriter, TrafficDatum trafficDatum) throws IOException {
+            if (trafficDatum == null) {
+                jsonWriter.nullValue();
+                return;
+            }
+            jsonWriter.beginArray();
+            jsonWriter.value(trafficDatum.getLocationName());
+            jsonWriter.value(trafficDatum.speedAndMaybeIncident());
+            jsonWriter.endArray();
+        }
+    
+        @Override
+        public TrafficDatum read(JsonReader jsonReader) throws IOException {
+            if (jsonReader.peek() == JsonToken.NULL) {
+                jsonReader.nextNull();
+                return null;
+            }
+            String token = jsonReader.nextString();
+            String[] parts = token.split(",");
+            TrafficDatum datum = new TrafficDatum();
+            String locationName = parts[0];
+            String[] speedAndMaybeIncident = token.split("-");
+            int speed = Integer.parseInt(speedAndMaybeIncident[0]);
+            datum.setLocationName(locationName);
+            datum.setSpeed(speed);
+            if (speedAndMaybeIncident.length > 1) {
+                datum.setIncident(speedAndMaybeIncident[1]);
+            }
+            return datum;
+        }
     }
 }
